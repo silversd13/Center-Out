acbd511988ea5badc5f940ee420a10c4ef364568
silversd13 <silversd13@gmail.com>
2019-03-18 13:25:44 -0700

    fixed bug in skipping inter-trial-interval

--- TaskCode/GetParams.m
+++ TaskCode/GetParams.m
@@ -18,10 +18,10 @@ end
 
 %% Control
 Params.Gain             = 1;
-Params.CenterReset      = true;
-Params.Assistance       = 0; % value btw 0 and 1, 1 full assist
-Params.CLDA.Type        = 2; % 0-none, 1-refit, 2-smooth batch, 3-RML
-Params.CLDA.AdaptType   = 'none'; % {'none','linear'}, affects assistance & lambda for rml
+Params.CenterReset      = false;
+Params.Assistance       = .2; % value btw 0 and 1, 1 full assist
+Params.CLDA.Type        = 3; % 0-none, 1-refit, 2-smooth batch, 3-RML
+Params.CLDA.AdaptType   = 'linear'; % {'none','linear'}, affects assistance & lambda for rml
 Params.InitializationMode = 3; % 1-imagined mvmts, 2-shuffled imagined mvmts, 3-choose dir
 
 %% Current Date and Time
@@ -76,12 +76,12 @@ Params.SerialSync = false;
 Params.SyncDev = '/dev/ttyS1';
 Params.BaudRate = 115200;
 
-Params.ArduinoSync = false;
+Params.ArduinoSync = true;
 
 %% Timing
 Params.ScreenRefreshRate = 10; % Hz
 Params.UpdateRate = 10; % Hz
-Params.BaselineTime = 1; % secs
+Params.BaselineTime = 60; % secs
 
 %% Targets
 Params.TargetSize = 50;
@@ -118,8 +118,8 @@ if Params.ControlMode>=3,
     Params.KF.W = [...
         0       0       0       0       0;
         0       0       0       0       0;
-        0       0       500     0       0;
-        0       0       0       500     0;
+        0       0       250   0       0;
+        0       0       0       250   0;
         0       0       0       0       0];
     Params.KF.P = eye(5);
     Params.KF.InitializationMode = Params.InitializationMode; % 1-imagined mvmts, 2-shuffled
@@ -136,9 +136,9 @@ Params.DrawVelCommand.Rect = [-425,-425,-350,-350];
 
 %% Trial and Block Types
 Params.NumImaginedBlocks    = 0;
-Params.NumAdaptBlocks       = 6;
-Params.NumFixedBlocks       = 2;
-Params.NumTrialsPerBlock    = length(Params.ReachTargetAngles);
+Params.NumAdaptBlocks       = 10;
+Params.NumFixedBlocks       = 0;
+Params.NumTrialsPerBlock    = 2*length(Params.ReachTargetAngles);
 Params.TargetSelectionFlag  = 1; % 1-pseudorandom, 2-random
 switch Params.TargetSelectionFlag,
     case 1, Params.TargetFunc = @(n) mod(randperm(n),Params.NumReachTargets)+1;
@@ -173,7 +173,7 @@ switch Params.CLDA.AdaptType,
             case 3, % RML
             Params.CLDA.DeltaAssistance = ... % linearly decrease assistance
                 Params.Assistance...
-                /(Params.NumAdaptBlocks*Params.NumTrialsPerBlock);
+                /((Params.NumAdaptBlocks-1)*Params.NumTrialsPerBlock);
             otherwise, % none or refit
             Params.CLDA.DeltaAssistance = 0;
         end
@@ -181,10 +181,10 @@ end
 
 %% Hold Times
 Params.TargetHoldTime = .1;
-Params.InterTrialInterval = 1;
+Params.InterTrialInterval = 4;
 Params.InstructedDelayTime = 0;
-Params.MaxStartTime = 10;
-Params.MaxReachTime = 10;
+Params.MaxStartTime = 15;
+Params.MaxReachTime = 15;
 Params.InterBlockInterval = 10; % 0-10s, if set to 10 use instruction screen
 Params.ImaginedMvmtTime = 4;
 
@@ -198,7 +198,7 @@ Params.ErrorSoundFs = 8192;
 sound(0*Params.ErrorSound,Params.ErrorSoundFs)
 
 %% BlackRock Params
-Params.GenNeuralFeaturesFlag = true;
+Params.GenNeuralFeaturesFlag = false;
 Params.ZscoreRawFlag = true;
 Params.ZscoreFeaturesFlag = false;
 Params.SaveProcessed = false;
